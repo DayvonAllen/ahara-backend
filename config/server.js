9ad90a06e1555ec934248c14fa0f5298ca4ae856
DayvonAllen <dayvonallen93@outlook.com>
2021-07-31 18:54:33 -0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ca86da6847fc46be64daf16008f353f5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'b2e75b70-1cfb-4814-b941-1c124345446a'
+};
